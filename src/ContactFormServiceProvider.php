4b676c6313ea63a04c231d687594a47f8ab40411
coder-manjeet <codermanjeet@gmail.com>
2025-02-02 12:42:49 +0530

    initial setup

---
+++ src/ContactFormServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CoderManjeet\LaravelContactForm;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContactFormServiceProvider extends ServiceProvider
+{
+    /**
+     * This function is responsible for bootstrapping the ContactFormServiceProvider.
+     * It loads routes, views, and optionally migrations for the package.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+
+    public function register() {
+        // 
+    }
+}
